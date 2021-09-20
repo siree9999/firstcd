465acca5be2077da5c6d8af814e0e25c67b53df5
Mona Lisa <monalisa@users.noreply.github.com>
2021-09-20 17:22:40 +0000

    learning about go basics

---
+++ goBasicsChallenge01.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    // var then variable name then variable type
+    var first,city,state string = "Siree","Minneapolis","MN"
+    
+    fmt.Println("This is", first + " from " + city + " " + state)
+    }
